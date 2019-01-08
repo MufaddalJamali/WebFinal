08d528d1401128646d0947386d702fb249708061
MufaddalJamali <mufaddalhussain72@gmail.com>
2019-01-08 11:25:07 +0500

    Final

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+
+
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBMum9lXQE_9EhcymlzKW9c1z71hfaveE0",
+    authDomain: "fir-app-ba572.firebaseapp.com",
+    databaseURL: "https://fir-app-ba572.firebaseio.com",
+    projectId: "fir-app-ba572",
+    storageBucket: "fir-app-ba572.appspot.com",
+    messagingSenderId: "1035842607140"
+};
